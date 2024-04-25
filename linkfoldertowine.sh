118550a97c0a6d2ed8de7c3172ea0e987b71c7cb
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-04-25 12:16:39 +0700

    Add files via upload

--- linkfoldertowine.sh
+++ linkfoldertowine.sh
@@ -9,46 +9,32 @@ read -p $'\e[1;37mEnter the folder name you want, it will appear on drive C and 
 echo -e "\e[0;33m--------------------"
 
 if [ ! -d "$external" ]; then
-    echo -e "\e[91mAborted because you did not enter the Micro SD card or USB path."
+    echo -e "\e[91mAborted because you did not enter the correct Micro SD card or USB path."
+    echo -e "\e[0;33m--------------------"
     exit 1
 fi
 
 if [ -z "$wine" ]; then
     echo -e "\e[91mAborted because you did not enter the correct Wine version."
+    echo -e "\e[0;33m--------------------"
     exit 1
 fi
 
 if [ -z "$name" ]; then
     echo -e "\e[91mAborted because you did not enter a new folder name to link your Micro SD card or USB to Wine."
+    echo -e "\e[0;33m--------------------"
     exit 1
 fi
 
 if [ ! -f "/data/data/com.termux/files/usr/glibc/$wine" ]; then
-    read -p $'\e[1;37mDetected that you have not initially set up Wine or you have entered the Wine version incorrectly. Do you want to continue? Enter "1" to continue or else to cancel: \e[0m' choosed
-    echo -e "\e[0;33m--------------------"
-    if [$choosed == 1]; then
-        mkdir "/data/data/com.termux/files/usr/glibc/$wine/.wine/drive_c"
-        if [ ! -f "/data/data/com.termux/files/usr/glibc/$wine/.wine/drive_c" ]; then
-        echo -e "\e[0;32mKeep going..."
-        echo -e "\e[0;33m--------------------"
-        else
-        echo -e "\e[0;31mCannot continue because the folder cannot be created."
-        exit 1
-        fi
-    else
-      echo -e "\e[0;31mCanceled."
-      exit 1
-    fi
+    echo -e "\e[0;31mLooks like you haven't started Wine yet, run it and try again. Or you did not import the correct version of Wine. Please check back."
+    exit 1
 fi
 
 ln -s "$external" "$PREFIX/glibc/$wine/.wine/drive_c/$name"
 
 if [ ! -f "/data/data/com.termux/files/usr/glibc/$wine/.wine/drive_c/$name" ]; then
-    read -p $'\e[1;37mDone! Do you want to run Mobox now? \e[0m' mobox
-    if [$mobox == 1]; then
-        cd
-        mobox
-    fi
+    echo -e "\e[1;37mDone!"
 else
     echo -e "\e[0;31mError! An error occurred. Please try again later."
 fi
