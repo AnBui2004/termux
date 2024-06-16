05f65052ffe10637da5a31eb43f78bb5629b1fd0
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-06-16 11:13:17 +0700

    Add files via upload

---
+++ shizuku.sh
@@ -0,0 +1,4 @@
+curl https://raw.githubusercontent.com/AnBui2004/termux/main/rish -o rish
+curl https://github.com/AnBui2004/termux/raw/main/rish_shizuku.dex -o rish_shizuku.dex
+chmod +rwx rish
+chmod +rwx rish_shizuku.dex
