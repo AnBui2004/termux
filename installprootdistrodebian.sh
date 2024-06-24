142ac1bb9bc1992ac819dbba0f94ec03440d5039
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-06-23 20:49:15 -0700

    Add files via upload

--- installprootdistrodebian.sh
+++ installprootdistrodebian.sh
@@ -1,7 +1,19 @@
+clear
+echo -e '\e[1;37mUpdating packages...\e[0m'
 apt update
 apt upgrade -y
+clear
+echo -e '\e[1;37mInstalling packages...\e[0m'
 pkg install x11-repo termux-am termux-tools -y
+clear
+echo -e '\e[1;37mPlease allow Termux access to storage...\e[0m'
 termux-setup-storage
+clear
+echo -e '\e[1;37mInstalling proot-distro ...\e[0m'
 pkg install proot-distro -y
+clear
+echo -e '\e[1;37mInstalling Debian...\e[0m'
 proot-distro install debian
+clear
+echo -e '\e[1;37mLogging in to Debian...\e[0m'
 proot-distro login debian
