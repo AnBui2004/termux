86b7da19c44ecbbc99bf719777eb39c03cca6b88
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-06-27 10:59:42 +0700

    Add files via upload

---
+++ idea.sh
@@ -0,0 +1,27 @@
+clear
+echo -e '\e[1;37mUpdating packages...\e[0m'
+apt update
+apt upgrade -y
+clear
+echo -e '\e[1;37mInstalling packages...\e[0m'
+apt install wget xterm fluxbox tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer thunar -y
+clear
+echo -e '\e[1;37mDownloading vscode...\e[0m'
+wget -O idea.tar.gz https://download-cdn.jetbrains.com/idea/ideaIU-2024.1.4-aarch64.tar.gz
+clear
+echo -e '\e[1;37mInstalling vscode...\e[0m'
+tar -xvzf idea.tar.gz
+rm idea.tar.gz
+clear
+echo -e '\e[1;37mJust a moment...\e[0m'
+apt-get --fix-missing install -y
+apt --fix-broken install -y
+wget -O idea.sh https://raw.githubusercontent.com/AnBui2004/termux/main/startidea.sh
+chmod +rwx idea.sh
+clear
+echo -e '\e[1;37mSetup VNC Server\e[0m'
+vncpasswd
+clear
+echo -e '\e[1;37mDone!\e[0m'
+echo -e '\e[1;37mUse this command to start vscode: "./idea.sh"\e[0m'
+echo -e '\e[1;37mUse this command to browse files: "thunar"\e[0m'
---
+++ startidea.sh
@@ -0,0 +1,3 @@
+cd idea-IU-241.18034.62
+cd bin
+./idea.sh
