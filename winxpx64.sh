5568abc49a6bc4e53baf556efe5567de300bab2f
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-08-19 01:23:26 +0700

    Add files via upload

--- winxpx64.sh
+++ winxpx64.sh
@@ -4,7 +4,7 @@ curl -o installqemu.sh https://raw.githubusercontent.com/AnBui2004/termux/main/i
 chmod +rwx installqemu.sh
 ./installqemu.sh
 rm installqemu.sh
-pkg install p7zip-full -y
+pkg install p7zip -y
 clear
 echo -e '\e[1;37mDownloading file...\e[0m'
 wget -O a.7z https://archive.org/download/windows-xp.-7z_202408/Windows%20XP.7z
@@ -15,8 +15,10 @@ mv XP.qcow2 /storage/emulated/0/VM
 rm a.7z
 clear
 echo -e '\e[1;37mJust a moment...\e[0m'
-echo qemu-system-x86_64 -cpu core2duo,+avx -usb -device usb-kbd -device usb-tablet -smp sockets=1,cores=4,threads=1 -m 512M -hda /storage/emulated/0/Documents/XP.qcow2 -vga vmware -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :2 > startxp.sh
+echo qemu-system-x86_64 -cpu core2duo,+avx -usb -device usb-kbd -device usb-tablet -smp sockets=1,cores=4,threads=1 -m 512M -hda /storage/emulated/0/VM/XP.qcow2 -vga vmware -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :2 > startxp.sh
 chmod +rwx startxp.sh
 clear
 echo -e '\e[1;37mDone!\e[0m'
 echo -e '\e[1;37mUse this command to run: "./startxp.sh"\e[0m'
+echo -e '\e[1;37m----------\e[0m'
+echo -e '\e[1;37mThe necessary files are in the VM folder on your phone. Please do not delete the files there if you still use them.\e[0m'
