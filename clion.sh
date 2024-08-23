cdf0cf4b3f7282fa285d2ca2c1b943aa29b0d99f
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-08-23 10:16:13 +0700

    Add files via upload

---
+++ clion.sh
@@ -0,0 +1,27 @@
+clear
+echo -e '\e[1;37mUpdating packages...\e[0m'
+apt update
+apt upgrade -y
+clear
+echo -e '\e[1;37mInstalling packages...\e[0m'
+apt install wget xterm fluxbox tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer thunar -y
+clear
+echo -e '\e[1;37mDownloading clion...\e[0m'
+wget -O clion.tar.gz https://download-cdn.jetbrains.com/cpp/CLion-2024.2.0.1-aarch64.tar.gz
+clear
+echo -e '\e[1;37mInstalling clion...\e[0m'
+tar -xvzf clion.tar.gz
+rm clion.tar.gz
+clear
+echo -e '\e[1;37mJust a moment...\e[0m'
+apt-get --fix-missing install -y
+apt --fix-broken install -y
+wget -O clion.sh https://raw.githubusercontent.com/AnBui2004/termux/main/startclion.sh
+chmod +rwx clion.sh
+clear
+echo -e '\e[1;37mSetup VNC Server\e[0m'
+vncpasswd
+clear
+echo -e '\e[1;37mDone!\e[0m'
+echo -e '\e[1;37mUse this command to start CLion: "./clion.sh"\e[0m'
+echo -e '\e[1;37mUse this command to browse files: "thunar"\e[0m'
---
+++ startclion.sh
@@ -0,0 +1,3 @@
+cd clion-2024.2.0.1
+cd bin
+./clion.sh
