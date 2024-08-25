00f5aea4b2f4fbb9a6bc37678168384507587e50
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-08-25 17:03:04 +0700

    Update win2000.sh

--- win2000.sh
+++ win2000.sh
@@ -11,14 +11,14 @@ wget -O a.7z http://192.168.1.111:8080/api/Windows%202000.7z
 7z x a.7z
 mkdir /storage/emulated/0/VM
 chmod +rwx /storage/emulated/0/VM
-mv Win95.qcow2 /storage/emulated/0/VM
+mv 2000.img /storage/emulated/0/VM
 rm a.7z
 clear
 echo -e '\e[1;37mJust a moment...\e[0m'
-echo qemu-system-x86_64 -cpu qemu32 -smp sockets=1,cores=1,threads=1 -m 128M -hda /storage/emulated/0/VM/Win95.qcow2 -vga cirrus -device sb16 -device pcnet,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :2 > start95.sh
-chmod +rwx start95.sh
+echo qemu-system-x86_64 -cpu qemu32 -smp sockets=1,cores=1,threads=1 -m 256M -hda /storage/emulated/0/VM/2000.img -vga cirrus -device ac97 -device ne2k_pci,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -vnc :2 > start2k.sh
+chmod +rwx start2k.sh
 clear
 echo -e '\e[1;37mDone!\e[0m'
-echo -e '\e[1;37mUse this command to run: "./start95.sh"\e[0m'
+echo -e '\e[1;37mUse this command to run: "./start2k.sh"\e[0m'
 echo -e '\e[1;37m----------\e[0m'
 echo -e '\e[1;37mThe necessary files are in the VM folder on your phone. Please do not delete the files there if you still use them.\e[0m'
