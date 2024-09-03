df9c88426d4492817c16ee0e88c9f1374c6f6a8b
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-09-03 13:49:53 +0700

    Add files via upload

---
+++ windowsserver2003x64.sh
@@ -0,0 +1,24 @@
+clear
+echo -e '\e[1;37mInstalling packages...\e[0m'
+curl -o installqemu.sh https://raw.githubusercontent.com/AnBui2004/termux/main/installqemu.sh
+chmod +rwx installqemu.sh
+./installqemu.sh
+rm installqemu.sh
+pkg install p7zip -y
+clear
+echo -e '\e[1;37mDownloading file...\e[0m'
+wget -O a.7z http://192.168.1.111:8080/api/Windows%20Server%202003%20x64.7z
+7z x a.7z
+mkdir /storage/emulated/0/VM
+chmod +rwx /storage/emulated/0/VM
+mv WS200364.qcow2 /storage/emulated/0/VM
+rm a.7z
+clear
+echo -e '\e[1;37mJust a moment...\e[0m'
+echo qemu-system-x86_64 -drive file=/storage/emulated/0/VM/WS200364.qcow2,aio=threads,cache=unsafe -cpu core2duo -smp sockets=1,cores=4,threads=1 -m 756M -vga std -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi -rtc base=2022-01-02t00:00:00 -device ac97 -vnc :2 -usb -device usb-tablet > start2003.sh
+chmod +rwx start2003.sh
+clear
+echo -e '\e[1;37mDone!\e[0m'
+echo -e '\e[1;37mUse this command to run: "./start2003.sh"\e[0m'
+echo -e '\e[1;37m----------\e[0m'
+echo -e '\e[1;37mThe necessary files are in the VM folder on your phone. Please do not delete the files there if you still use them.\e[0m'
