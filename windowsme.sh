440bcf2bfa924982651572ef1b3768ac9b7b8cfc
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-09-09 09:34:52 +0700

    Add files via upload

---
+++ windowsme.sh
@@ -0,0 +1,24 @@
+clear
+echo -e '\e[1;37mInstalling packages...\e[0m'
+curl -o installqemu.sh https://raw.githubusercontent.com/AnBui2004/termux/main/installqemu.sh
+chmod +rwx installqemu.sh
+./installqemu.sh
+rm installqemu.sh
+pkg install p7zip -y
+clear
+echo -e '\e[1;37mDownloading file...\e[0m'
+wget -O a.7z http://192.168.1.111:8080/api/Windows%20ME.7z
+7z x a.7z
+mkdir /storage/emulated/0/VM
+chmod +rwx /storage/emulated/0/VM
+mv ME.qcow2 /storage/emulated/0/VM
+rm a.7z
+clear
+echo -e '\e[1;37mJust a moment...\e[0m'
+echo qemu-system-x86_64 -cpu core2duo -smp sockets=1,cores=4,threads=1 -m 256M -hda /storage/emulated/0/Documents/ME.qcow2 -vga cirrus -device ES1370 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -usb -device usb-tablet -device usb-kbd -vnc :2 > startme.sh
+chmod +rwx startme.sh
+clear
+echo -e '\e[1;37mDone!\e[0m'
+echo -e '\e[1;37mUse this command to run: "./startme.sh"\e[0m'
+echo -e '\e[1;37m----------\e[0m'
+echo -e '\e[1;37mThe necessary files are in the VM folder on your phone. Please do not delete the files there if you still use them.\e[0m'
