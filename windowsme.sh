288d90be0836c0b642ae1ef6f907ed238884a922
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-09-09 21:15:56 +0700

    Update windowsme.sh

--- windowsme.sh
+++ windowsme.sh
@@ -7,7 +7,7 @@ rm installqemu.sh
 pkg install p7zip -y
 clear
 echo -e '\e[1;37mDownloading file...\e[0m'
-wget -O a.7z http://192.168.1.111:8080/api/Windows%20ME.7z
+wget -O a.7z https://archive.org/download/windows-me.-7z_202409/Windows%20ME.7z
 7z x a.7z
 mkdir /storage/emulated/0/VM
 chmod +rwx /storage/emulated/0/VM
