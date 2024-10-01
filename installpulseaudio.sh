81229c9d37c019c912eb51b342d520cd779e9f98
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-10-01 11:30:48 +0700

    Create installpulseaudio.sh

---
+++ installpulseaudio.sh
@@ -0,0 +1,10 @@
+apt update
+apt upgrade -y
+pkg install x11-repo -y
+pkg install pulseaudio -y
+export PULSE_SERVER=127.0.0.1
+echo pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1 > sound.sh
+chmod +rwx sound.sh
+pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+echo -e '\e[1;37mDone!\e[0m'
+echo -e '\e[1;37mIf the Termux process was stopped before and you reopen Termux or it doesn't work, run this command: "./sound.sh".\e[0m'
