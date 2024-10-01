b884eb7e6d8d273dd776c8f54db0570fe81ab754
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-10-01 12:10:10 +0700

    Update installpulseaudio.sh

--- installpulseaudio.sh
+++ installpulseaudio.sh
@@ -6,6 +6,7 @@ export PULSE_SERVER=127.0.0.1
 curl -o sound.sh https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/runpulseaudio.sh
 chmod +rwx sound.sh
 ./sound.sh
+echo /data/data/com.termux/files/home/sound.sh >> /data/data/com.termux/files/usr/etc/bash.bashrc
 clear
 echo -e '\e[1;37mDone!\e[0m'
-echo -e '\e[1;37mIf the Termux process was stopped before and you reopen Termux or it does not work, run this command: "./sound.sh".\e[0m'
+echo -e '\e[1;37mIf it does not work, run this command: "./sound.sh".\e[0m'
