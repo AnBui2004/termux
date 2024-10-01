b23cda79dce0053288fd9f5cdc144d3dd9ec7bee
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-10-01 11:39:14 +0700

    Update installpulseaudio.sh

--- installpulseaudio.sh
+++ installpulseaudio.sh
@@ -6,5 +6,6 @@ export PULSE_SERVER=127.0.0.1
 echo pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1 > sound.sh
 chmod +rwx sound.sh
 pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1
+clear
 echo -e '\e[1;37mDone!\e[0m'
 echo -e '\e[1;37mIf the Termux process was stopped before and you reopen Termux or it does not work, run this command: "./sound.sh".\e[0m'
