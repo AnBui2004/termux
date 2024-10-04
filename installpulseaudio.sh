1d7ece9dd7565195f11a21d47c6c165190ecbbd1
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-10-04 23:47:10 +0700

    Update installpulseaudio.sh

--- installpulseaudio.sh
+++ installpulseaudio.sh
@@ -6,6 +6,7 @@ export PULSE_SERVER=127.0.0.1
 curl -o sound.sh https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/runpulseaudio.sh
 chmod +rwx sound.sh
 ./sound.sh
+sed -i '/sound.sh/d' /data/data/com.termux/files/usr/etc/bash.bashrc
 echo /data/data/com.termux/files/home/sound.sh >> /data/data/com.termux/files/usr/etc/bash.bashrc
 clear
 echo -e '\e[1;37mDone!\e[0m'
