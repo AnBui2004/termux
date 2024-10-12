0de3b2d8feb7d2204fb8f6cfd4f5a0749bed9be5
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-10-12 12:27:36 +0700

    Add files via upload

--- setupwindows10iotltsc2021one.sh
+++ setupwindows10iotltsc2021one.sh
@@ -34,7 +34,7 @@ chmod +rwx setup121024.sh
 echo qemu-system-x86_64 -M q35,hmat=on -usb -device usb-tablet -device usb-kbd -cpu qemu64,+avx,+avx-ifma,+avx-ne-convert,+avx-vnni,+avx-vnni-int8,+avx2,+avx512-4fmaps,+avx512-4vnniw,+avx512-bf16,+avx512-fp16,+avx512-vp2intersect,+avx512-vpopcntdq,+avx512bitalg,+avx512bw,+avx512cd,+avx512dq,+avx512er,+avx512f,+avx512ifma,+avx512pf,+avx512vbmi,+avx512vbmi2,+avx512vl,+avx512vnni,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=6,threads=1 -m 4096M -overcommit mem-lock=off -drive file=/storage/emulated/0/VM/W10IoTLTSC2021.qcow2,aio=threads,cache=unsafe,if=none,id=hda -device virtio-blk-pci,drive=hda -vga none -device virtio-gpu-pci,max_hostmem=128M -device intel-hda -device hda-duplex -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -device intel-iommu -vnc :2 > start102021vm.sh
 chmod +rwx start102021vm.sh
 cd ../
-echo '/root/setup121024.sh' >> /etc/profile
+echo '/root/setup121024.sh' >> ./etc/profile
 cd
 echo 'proot-distro login debian' > start102021.sh
 chmod +rwx start102021.sh
