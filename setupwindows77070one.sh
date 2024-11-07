a842ef8627890c7402d74725a09992ac574f5441
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-11-07 05:09:31 -0800

    Add files via upload

--- setupwindows77070one.sh
+++ setupwindows77070one.sh
@@ -29,12 +29,12 @@ echo -e '\e[1;37mJust a sec...\e[0m'
 cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root
 curl -o "setup"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setupwindows"$setname"two.sh
 chmod +rwx "setup"$setname".sh"
-echo qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=4,threads=1 -m 2048M -overcommit mem-lock=off -drive file=/storage/emulated/0/VM/VMW77070.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device intel-hda -device hda-duplex -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -rtc base=2009-03-24t00:00:00 -vnc :2 > "start"$setname"vm.sh"
+echo qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu qemu64,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=4,threads=1 -m 2048M -overcommit mem-lock=off -drive file=/storage/emulated/0/VM/W77070.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device intel-hda -device hda-duplex -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -rtc base=2009-03-24t00:00:00 -vnc :2 > "start"$setname"vm.sh"
 chmod +rwx "start"$setname"vm.sh"
 cd ../
 echo "/root/setup"$setname".sh" >> ./etc/profile
 cd
-echo 'sed -i '/start"$setname"/d' /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' > "start"$setname"vm.sh"
+echo 'sed -i '/start"$setname"/d' /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' > "start"$setname".sh"
 echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1 && echo '/root/start"$setname".sh' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' >> start"$setname".sh
 echo 'proot-distro login debian' >> start"$setname".sh
 chmod +rwx start"$setname".sh
--- setupwindows77070two.sh
+++ setupwindows77070two.sh
@@ -11,17 +11,17 @@ clear
 echo -e '\e[1;37mJust a sec...\e[0m'
 if [ -e "/storage/emulated/0/VM/"$diskfilename"" ]; then
     cd
-    curl -o "start"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/start"$setname".sh
-    chmod +rwx start"$setname".sh
+    curl -o "start"$setname".sh" "https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/start"$setname".sh"
+    chmod +rwx "start"$setname".sh"
     echo "/root/start"$setname".sh" >> /etc/profile
     clear
     echo -e '\e[1;37mDone!\e[0m'
-    echo -e ""\e[1;37mUse this command to run: "./start"$setname".sh"\e[0m"
+    echo -e "\e[1;37mUse this command to run: "./start"$setname".sh"\e[0m"
     echo -e '\e[1;37m----------\e[0m'
     echo -e '\e[1;37mThe necessary files are in the VM folder on your phone. Please do not delete the files there if you still use them.\e[0m'
 else
     clear
-    sed -i '/start"$setname"/d' /etc/profile
+    sed -i "/start"$setname"/d" /etc/profile
     cd
     rm start"$setname"vm.sh
     rm /storage/emulated/0/VM/"$diskfilename"
@@ -30,5 +30,5 @@ else
     echo -e "\e[0;33m--------------------"
     echo -e "\e[91mError! An error occurred. Please try again later."
 fi
-rm setup"$setname".sh
+rm "setup"$setname".sh"
 exit
