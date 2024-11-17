343cfb828f459f363a99b34bbd4b047a427cb832
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-11-17 12:35:30 +0700

    Add files via upload

---
+++ setupwl4074one.sh
@@ -0,0 +1,44 @@
+clear
+setname='wl4074'
+echo -e '\e[1;37mInstalling packages...\e[0m'
+getpermisionsdcard=$(ls -l /sdcard/)
+if [ "$getpermisionsdcard" == "" ]; then
+yes y | termux-setup-storage
+fi
+apt update
+apt upgrade -y
+apt install x11-repo -y
+apt install proot-distro p7zip wget pulseaudio -y
+clear
+echo -e '\e[1;37mSetting up Pulseaudio...\e[0m'
+export PULSE_SERVER=127.0.0.1
+clear
+echo -e '\e[1;37mDownloading file...\e[0m'
+mkdir /storage/emulated/0/VM
+chmod +rwx /storage/emulated/0/VM
+cd /storage/emulated/0/VM
+wget -O a.7z http://192.168.1.111:8080/api/Windows%20Longhorn%204074.7z
+7z x a.7z
+rm a.7z
+cd
+clear
+echo -e '\e[1;37mInstalling Debian...\e[0m'
+proot-distro install debian
+clear
+echo -e '\e[1;37mJust a sec...\e[0m'
+cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root
+curl -o "setup"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setup"$setname"two.sh
+chmod +rwx "setup"$setname".sh"
+echo qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu qemu32 -smp sockets=1,cores=2,threads=1 -m 1024M -overcommit mem-lock=off -drive file=/storage/emulated/0/VM/WL4074.qcow2,aio=threads,cache=unsafe -vga cirrus -device ac97 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -rtc base=2004-04-25t00:00:00 -vnc :2 > "start"$setname"vm.sh"
+chmod +rwx "start"$setname"vm.sh"
+cd ../
+echo "/root/setup"$setname".sh" >> ./etc/profile
+cd
+echo 'sed -i '/start"$setname"/d' /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' > "start"$setname".sh"
+echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1 && echo '/root/start"$setname".sh' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' >> start"$setname".sh
+echo 'proot-distro login debian' >> start"$setname".sh
+chmod +rwx start"$setname".sh
+clear
+echo -e '\e[1;37mLogin to Debian...\e[0m'
+proot-distro login debian
+rm /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root/setup"$setname".sh
