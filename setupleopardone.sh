08665f1e5bf90c40e639654eb01314bff6e67e42
An Bui <91354810+AnBui2004@users.noreply.github.com>
2024-12-02 11:41:57 +0700

    Add files via upload

---
+++ setupleopardone.sh
@@ -0,0 +1,45 @@
+clear
+setname='leopard'
+echo -e '\e[1;37mInstalling packages...\e[0m'
+getpermisionsdcard=$(ls -l /sdcard/)
+if [ "$getpermisionsdcard" == "" ]; then
+yes y | termux-setup-storage
+fi
+apt update
+apt upgrade -y
+apt install x11-repo -y
+apt install proot-distro p7zip wget pulseaudio -y
+clear
+echo -e '\e[1;37mSetting up Pulseaudio...\e[0m'
+export PULSE_SERVER=127.0.0.1
+clear
+echo -e '\e[1;37mDownloading file...\e[0m'
+mkdir /storage/emulated/0/VM
+chmod +rwx /storage/emulated/0/VM
+cd /storage/emulated/0/VM
+wget -O a.7z http://192.168.1.111:8080/api/Mac%20OS%20X%20Leopard.7z
+7z x a.7z
+rm a.7z
+cd
+clear
+echo -e '\e[1;37mInstalling Debian...\e[0m'
+proot-distro install debian
+clear
+echo -e '\e[1;37mJust a sec...\e[0m'
+cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root
+curl -o "setup"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setup"$setname"two.sh
+chmod +rwx "setup"$setname".sh"
+echo 'qemu-system-ppc -L pc-bios -M mac99,via=pmu -cpu G4 -m 1024M -device VGA,edid=on,vgamem_mb=64,xres=1280,yres=720 -g 1280x720x32 -hda /storage/emulated/0/VM/Leopard.qcow2 -netdev user,id=n0 -device sungem,netdev=n0,mac=52:54:00:12:34:57 -accel tcg,thread=single,tb-size=2048 -device usb-audio -device usb-tablet -boot c -vnc :2' > "start"$setname"vm.sh"
+chmod +rwx "start"$setname"vm.sh"
+cd ../
+echo "/root/setup"$setname".sh" >> ./etc/profile
+cd
+echo 'sed -i '/start"$setname"/d' /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' > "start"$setname".sh"
+echo 'pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1' >> start"$setname".sh
+echo 'echo '/root/start"$setname".sh' >> /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc/profile' >> start"$setname".sh
+echo 'proot-distro login debian' >> start"$setname".sh
+chmod +rwx start"$setname".sh
+clear
+echo -e '\e[1;37mLogin to Debian...\e[0m'
+proot-distro login debian
+rm /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root/setup"$setname".sh
