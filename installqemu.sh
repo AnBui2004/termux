4b89267100c1a95a69c77800a4586809bacd1e4f
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-02-01 20:39:59 +0700

    Update installqemu.sh

--- installqemu.sh
+++ installqemu.sh
@@ -1,7 +1,21 @@
+clear
+echo -e '\e[1;37mUpdating packages...\e[0m'
 apt update
 apt upgrade -y
+clear
+echo -e '\e[1;37mInstalling packages...\e[0m'
 pkg install x11-repo -y
 pkg install termux-am termux-tools pulseaudio wget qemu-system-x86_64 -y
+clear
+echo -e '\e[1;37mInstalling libraries...\e[0m'
 pkg install libdw alsa-plugins jack2 -y
-termux-setup-storage
+clear
+echo -e '\e[1;37mDo you want to grant Termux access to your storage?\e[0m'
+getpermisionsdcard=$(ls -l /sdcard/)
+if [ "$getpermisionsdcard" == "" ]; then
+yes y | termux-setup-storage
+fi
+sleep 3
 export PULSE_SERVER=127.0.0.1
+clear
+echo -e '\\e[1;37mDone!\\e[0m'
