3b88f7f099a797989307369c6db16840f3ac0c7a
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-02-22 23:43:54 +0700

    Update setupnt31asone.sh

--- setupnt31asone.sh
+++ setupnt31asone.sh
@@ -17,7 +17,7 @@ echo -e '\e[1;37mDownloading file...\e[0m'
 mkdir /storage/emulated/0/VM
 chmod +rwx /storage/emulated/0/VM
 cd /storage/emulated/0/VM
-wget -O a.7z 'https://www.googleapis.com/drive/v3/files/1K2QzBsKWokecYG1IhvAzq8UVOFgWt9a1?alt=media&key=AIzaSyBg5cJ4QZF7kDgco9Sxrs-cPtV3F65bVSY'
+wget -O a.7z 'https://archive.org/download/windows-nt-3.1-advanced-server.-7z_202502nbab/Windows%20NT%203.1%20Advanced%20Server.7z'
 7z x a.7z
 rm a.7z
 cd
