92d427c912c42fe7a9e7dba31bf63861c1c5b109
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-03-05 15:51:09 +0700

    Update startsetup.sh

--- setupvm/31/startsetup.sh
+++ setupvm/31/startsetup.sh
@@ -2,7 +2,8 @@ cd
 setname='w31'
 diskfilename='Windows3.1.vdi'
 setfileurl='https://archive.org/download/windows-3.1.7znbab/Windows%203.1.7z'
-setqemucommand='qemu-system-x86_64 -cpu qemu32 -smp sockets=1,cores=1,threads=1 -m 128M -vga cirrus -device sb16 -device pcnet,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -monitor stdio -vnc :2'
+setqemucommand='
+qemu-system-x86_64 -cpu qemu32 -smp sockets=1,cores=1,threads=1 -m 128M -hda /storage/emulated/0/VM/w31/Windows3.1.vdi -vga cirrus -device sb16 -device pcnet,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -monitor stdio -vnc :2'
 osname="Windows_3.1"
 export setname
 export diskfilename
