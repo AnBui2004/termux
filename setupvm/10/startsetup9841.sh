8c15ba5bb5018eab1d7560bba3aed692bdc21a00
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-03-14 12:32:09 +0700

    Create startsetup9841.sh

---
+++ setupvm/10/startsetup9841.sh
@@ -0,0 +1,11 @@
+cd
+setname='109841'
+diskfilename='W109841.qcow2'
+setfileurl='http://localhost:8080/files/W109841.7z'
+setqemucommand='qemu-system-x86_64 -M q35 -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64,+avx,+avx2,+sse,+sse2,+sse4.1,+sse4.2,hv-relaxed -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M -drive file=/storage/emulated/0/VM/109841/W109841.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device ich9-intel-hda -device hda-duplex -device e1000e,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -device intel-iommu -monitor stdio -vnc :2'
+osname="Windows_10_build_9841"
+export setname
+export diskfilename
+export setfileurl
+export setqemucommand
+export osname
