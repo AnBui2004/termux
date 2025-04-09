10304ff816f15b746ba1ae0478e0289988011aec
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-04-09 21:19:41 +0700

    Update

--- setupqemu/setupqemuvm1.sh
+++ setupqemu/setupqemuvm1.sh
@@ -6,34 +6,37 @@ fi
 echo -e "\e[1;37mAutomatically go to next step after 3 seconds."
 sleep 3
 clear
-echo -e "\e[1;37m---------\/-----------"
-echo -e "\e[1;37mWarning: Please do not run any other commands when this setup begins. If you're running other commands and they haven't finished executing yet, press Ctrl + C now to cancel the setup immediately."
-echo -e "\e[1;37m---------/\-----------"
+echo -e "\e[1;37m[!] Warning!"
+echo -e "\e[1;37m-\e[0m"
+echo -e "\e[1;37mPlease do not run any other commands when this setup begins. If you're running other commands and they haven't finished executing yet, press Ctrl + C now to cancel the setup immediately."
+echo -e "\e[1;37m-\e[0m"
 echo -e "\e[1;37mAutomatically go to next step after 7 seconds and you agreed."
 sleep 7
 clear
-echo -e "\e[1;37m---------\/-----------"
+echo -e "\e[1;37m[!] Do not miss out!"
+echo -e "\e[1;37m-\e[0m"
 echo -e "\e[1;37mBy obtaining and running the virtual machine in any way, you agree to any Agreements and Policies provided by the vendor or owner of this operating system and other related things. You are responsible for any consequences that may arise from using the virtual machine. The owner of this script is not responsible for any consequences that may arise from using the virtual machine or you have edited these scripts. To disagree and cancel the setup, press Ctrl + C."
-echo -e "\e[1;37m---------/\-----------"
+echo -e "\e[1;37m-\e[0m"
 echo -e "\e[1;37mAutomatically go to next step after 10 seconds and you agreed."
 sleep 10
 clear
 source ./setup1.sh
-echo -e '\e[1;37mInstalling packages...\e[0m'
+echo -e '\e[1;37m[i] Installing packages...\e[0m'
 apt update
 yes y | apt upgrade -y
 apt install x11-repo -y
 apt install proot-distro p7zip wget pulseaudio -y
 clear
-echo -e '\e[1;37mSetting up Pulseaudio...\e[0m'
+echo -e '\e[1;37m[i] Setting up Pulseaudio...\e[0m'
 export PULSE_SERVER=127.0.0.1
 clear
 getpermisionsdcard=$(ls -l /sdcard/)
 if [ "$getpermisionsdcard" == "" ]; then
     clear
-    echo -e "\e[1;37m---------\/-----------"
+    echo -e "\e[1;37m[!] Canceled!"
+    echo -e "\e[1;37m-\e[0m"
     echo -e "\e[1;37mSetup was canceled because you did not grant Termux access to storage."
-    echo -e "\e[1;37m---------/\-----------"
+    echo -e "\e[1;37m-\e[0m"
     rm "setup1.sh"
     rm "setup2.sh"
     exit
@@ -48,18 +51,18 @@ if [ ! -d "/storage/emulated/0/VM/Shared" ]; then
 fi
 clear
 if [ ! -e "/storage/emulated/0/VM/"$setname"/"$diskfilename"" ]; then
-    echo -e '\e[1;37mDownloading disk image...\e[0m'
-    echo -e "\e[1;37m-"
+    echo -e '\e[1;37m[i] Downloading disk image...\e[0m'
+    echo -e "\e[1;37m-\e[0m"
     if [[ "$setfileurl" == *"pixeldrain"* || "$setfileurl" == *"google"* ]]; then
         echo -e "\e[1;37m[i] Downloading from high speed server."
     else
         echo -e "\e[1;37m[!] Downloading from slow speed server."
     fi
-    echo -e "\e[1;37m-"
+    echo -e "\e[1;37m-\e[0m"
     if [[ "$setfileurl" =~ "pixeldrain" ]]; then
         echo -e "\e[1;37m[!] You are limited to a maximum daily download size of 6GB from this server. If the download fails, please try again tomorrow."
     fi
-    echo -e "\e[1;37m-"
+    echo -e "\e[1;37m-\e[0m"
     cd /storage/emulated/0/VM/$setname
     wget -O file.7z $setfileurl
     7z x file.7z
@@ -68,23 +71,23 @@ if [ ! -e "/storage/emulated/0/VM/"$setname"/"$diskfilename"" ]; then
     if [ ! -e "/storage/emulated/0/VM/"$setname"/"$diskfilename"" ]; then
         clear
         echo -e "\e[1;37m[!] Unable to download"
-        echo -e "\e[1;37m-"
+        echo -e "\e[1;37m-\e[0m"
         if [[ "$setfileurl" =~ "archive.org" ]]; then
             echo -e "\e[1;37mThis item may be restricted by the Internet Archive. You can try downloading it manually as follows:"
             echo -e "\e[1;37m[1] Open your browser and go to https://archive.org/ ."
             echo -e "\e[1;37m[2] Sign in or create a new account."
             echo -e "\e[1;37m[3] Open this link on your browser to start the download:"
-            echo -e "\e[1;37m-"
+            echo -e "\e[1;37m-\e[0m"
             echo -e "\e[1;37m$setfileurl"
-            echo -e "\e[1;37m-"
+            echo -e "\e[1;37m-\e[0m"
             echo -e "\e[1;37m[4] Extract the downloaded file into $setname folder in VM folder on your phone (Internal storage/VM/$setname)."
             echo -e "\e[1;37m[5] Run this setup command again."
             echo -e "\e[1;37m-"
             echo -e "\e[1;37mOr join our community for help here: https://anbui.ovh/community ."
         else
             if [[ "$setfileurl" =~ "pixeldrain" ]]; then
-                echo -e "\e[1;37mYou are limited to a maximum daily download size of 6GB from this server. If the download fails, please try again tomorrow."
-                echo -e "\e[1;37m-"
+                echo -e "\e[1;37mSince this server limits you to a total of 6GB of downloads per day, this may have caused the download to fail. You can try again now or tomorrow."
+                echo -e "\e[1;37m-\e[0m"
                 echo -e "\e[1;37mOr join our community for help here: https://anbui.ovh/community ."
             else
                 echo -e "\e[1;37mPlease try again later or join our community for help here: https://anbui.ovh/community ."
@@ -96,10 +99,10 @@ if [ ! -e "/storage/emulated/0/VM/"$setname"/"$diskfilename"" ]; then
     fi
 fi
 clear
-echo -e '\e[1;37mInstalling Debian...\e[0m'
+echo -e '\e[1;37m[i] Installing Debian...\e[0m'
 proot-distro install debian
 clear
-echo -e '\e[1;37mJust a sec...\e[0m'
+echo -e '\e[1;37m[i] Just a sec...\e[0m'
 cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root
 curl -o "setup"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setupqemu/setupqemuvm2.sh
 chmod +rwx "setup"$setname".sh"
@@ -126,6 +129,6 @@ echo 'echo '/root/start"$setname".sh' >> /data/data/com.termux/files/usr/var/lib
 echo 'proot-distro login debian' >> start"$setname".sh
 chmod +rwx start"$setname".sh
 clear
-echo -e '\e[1;37mLogin to Debian...\e[0m'
+echo -e '\e[1;37m[i] Login to Debian...\e[0m'
 proot-distro login debian
 rm /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/root/setup"$setname".sh
--- setupqemu/setupqemuvm2.sh
+++ setupqemu/setupqemuvm2.sh
@@ -1,12 +1,12 @@
 clear
-echo -e '\e[1;37mInstalling packages...\e[0m'
+echo -e '\e[1;37m[i] Installing packages...\e[0m'
 sed -i "/setup"$setname"/d" /etc/profile
 apt update
 apt upgrade -y
 apt install pulseaudio tightvncserver qemu-system-x86 -y
 export PULSE_SERVER=127.0.0.1
 clear
-echo -e '\e[1;37mJust a sec...\e[0m'
+echo -e '\e[1;37m[i] Just a sec...\e[0m'
 if [ -e "/storage/emulated/0/VM/"$setname"/"$diskfilename"" ]; then
     cd
     curl -o "start"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setupqemu/startqemuvm.sh
@@ -15,19 +15,21 @@ if [ -e "/storage/emulated/0/VM/"$setname"/"$diskfilename"" ]; then
     sed -i -e "1isetname="$setname"" start"$setname".sh
     sed -i -e "1iosname='"$osname"'" start"$setname".sh
     clear
-    echo -e '\e[1;37mDone!\e[0m'
+    echo -e '\e[1;37m[i] Done!\e[0m'
+    echo -e '\e[1;37m-\e[0m'
     echo -e "\e[1;37mUse this command to run: "./start"$setname".sh"\e[0m"
-    echo -e '\e[1;37m----------\e[0m'
+    echo -e '\e[1;37m-\e[0m'
     echo -e '\e[1;37mThe necessary files are in the VM folder on your phone. Please do not delete the files there if you still use them.\e[0m'
 else
-    clear
     sed -i "/start"$setname"/d" /etc/profile
     cd
     rm start"$setname"vm.sh
     rm -r /storage/emulated/0/VM/"$setname"
     rm start"$setname".sh
     rm /data/data/com.termux/files/home/start"$setname".sh
-    echo -e "\e[0;33m--------------------"
+    clear
+    echo -e "\e[0;33m[!] Unsuccessful!"
+    echo -e "\e[1;37m-\e[0m"
     echo -e "\e[91mError! An error occurred. Please try again later."
 fi
 rm "setup"$setname".sh"
