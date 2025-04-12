66fa4c41c0f22b3d407f4a0270e312c06d3063a6
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-04-12 10:16:24 +0700

    Create startsetup8102-101.sh

---
+++ setupvm/8/startsetup8102-101.sh
@@ -0,0 +1,11 @@
+cd
+setname='88102'
+diskfilename='W88102.qcow2'
+setfileurl='http://192.168.1.111:8080/api/Windows%208%20build%208102.101.7z'
+setqemucommand='qemu-system-x86_64 -M pc -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64,+avx,+avx2,+sse,+sse2,+sse4.1,+sse4.2 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M -drive file=/storage/emulated/0/VM/88102/W88102.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device ich9-intel-hda -device hda-duplex -device e1000e,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -monitor stdio -vnc :2'
+osname="Windows_8_build_8102_101"
+export setname
+export diskfilename
+export setfileurl
+export setqemucommand
+export osname
