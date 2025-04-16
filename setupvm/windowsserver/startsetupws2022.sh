0d13ff63af6d708caa260d92f88a1548280b7568
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-04-16 10:18:33 +0700

    Create startsetupws2022.sh

---
+++ setupvm/windowsserver/startsetupws2022.sh
@@ -0,0 +1,11 @@
+cd
+setname='ws2022'
+diskfilename='WS2022.qcow2'
+setfileurl='http://192.168.1.111:8080/api/Windows%20Server%202022.7z'
+setqemucommand='qemu-system-x86_64 -M q35 -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64,+avx,+avx2,+sse,+sse2,+sse4.1,+sse4.2,hv-relaxed -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M,slots=4,maxmem=4097M -object memory-backend-ram,size=1024M,id=m0 -numa node,nodeid=0,memdev=m0,cpus=0 -object memory-backend-ram,size=1024M,id=m1 -numa node,nodeid=1,memdev=m1,cpus=1 -object memory-backend-ram,size=1024M,id=m2 -numa node,nodeid=2,memdev=m2,cpus=2 -object memory-backend-ram,size=1024M,id=m3 -numa node,nodeid=3,memdev=m3,cpus=3 -drive file=/storage/emulated/0/VM/ws2022/WS2022.qcow2,aio=threads,cache=unsafe,if=virtio -device qxl-vga,vgamem_mb=128 -device virtio-gpu-pci -device ich9-intel-hda -device hda-duplex -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -drive file=/storage/emulated/0/VM/ws2022/OVMF.fd,format=raw,readonly=on,if=pflash -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -device intel-iommu -monitor stdio -vnc :2'
+osname="Windows_Server_2022"
+export setname
+export diskfilename
+export setfileurl
+export setqemucommand
+export osname
