4dae10dccc3272bfbb92eacdc943f94f257d47a0
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-04-20 09:41:05 +0700

    Update

--- setupvm/10/startsetup1709.sh
+++ setupvm/10/startsetup1709.sh
@@ -1,7 +1,7 @@
 cd
 setname='101709'
 diskfilename='W101709.qcow2'
-setfileurl='https://www.googleapis.com/drive/v3/files/1Usvjxm6kxCyUW2nZiv-dC-CPZKlwVZOY?alt=media&key=AIzaSyBg5cJ4QZF7kDgco9Sxrs-cPtV3F65bVSY'
+setfileurl='https://archive.org/download/windows-10-1709.7znbab/Windows%2010%201709.7z'
 setqemucommand='qemu-system-x86_64 -M q35 -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64,+avx,+avx2,+sse,+sse2,+sse4.1,+sse4.2,hv-relaxed -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M,slots=4,maxmem=4097M -object memory-backend-ram,size=1024M,id=m0 -numa node,nodeid=0,memdev=m0,cpus=0 -object memory-backend-ram,size=1024M,id=m1 -numa node,nodeid=1,memdev=m1,cpus=1 -object memory-backend-ram,size=1024M,id=m2 -numa node,nodeid=2,memdev=m2,cpus=2 -object memory-backend-ram,size=1024M,id=m3 -numa node,nodeid=3,memdev=m3,cpus=3 -drive file=/storage/emulated/0/VM/101709/W101709.qcow2,aio=threads,cache=unsafe,if=none,id=hda -device virtio-blk-pci,drive=hda -device qxl-vga,vgamem_mb=128 -device ich9-intel-hda -device hda-duplex -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -drive file=/storage/emulated/0/VM/101709/OVMF.fd,format=raw,readonly=on,if=pflash -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -device intel-iommu -monitor stdio -vnc :2'
 osname="Windows_10_1709"
 export setname
