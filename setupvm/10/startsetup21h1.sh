bd09effccb8d9506e916c48b723b5c3f7c163a6d
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-04-21 19:05:42 +0700

    Update

--- setupvm/10/startsetup21h1.sh
+++ setupvm/10/startsetup21h1.sh
@@ -1,11 +1,11 @@
 cd
-setname='21h2'
-diskfilename='W1021H2.qcow2'
+setname='21h1'
+diskfilename='W1021H1.qcow2'
 setfileurl='https://192.168.1.111/Windows%2010%2021H1.7z'
 setfileurl2=''
 setfileurl3=''
 setqemucommand='qemu-system-x86_64 -M q35 -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64,+avx,+avx2,+sse,+sse2,+sse4.1,+sse4.2,hv-relaxed -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M,slots=4,maxmem=4097M -object memory-backend-ram,size=1024M,id=m0 -numa node,nodeid=0,memdev=m0,cpus=0 -object memory-backend-ram,size=1024M,id=m1 -numa node,nodeid=1,memdev=m1,cpus=1 -object memory-backend-ram,size=1024M,id=m2 -numa node,nodeid=2,memdev=m2,cpus=2 -object memory-backend-ram,size=1024M,id=m3 -numa node,nodeid=3,memdev=m3,cpus=3 -drive file=/storage/emulated/0/VM/21h1/W1021H1.qcow2,aio=threads,cache=unsafe,if=none,id=hda -device virtio-blk-pci,drive=hda -device qxl-vga,vgamem_mb=128 -device ich9-intel-hda -device hda-duplex -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -drive file=/storage/emulated/0/VM/21h1/OVMF.fd,format=raw,readonly=on,if=pflash -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -device intel-iommu -monitor stdio -vnc :2'
-osname="Windows_10_21H2"
+osname="Windows_10_21H1"
 export setname
 export diskfilename
 export setfileurl
