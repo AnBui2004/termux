475ff216017f3e67a01d184190d1394622afdce8
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-04-29 12:33:26 +0700

    Update

--- setupvm/11/startsetupiotltsc22621-1.sh
+++ setupvm/11/startsetupiotltsc22621-1.sh
@@ -3,7 +3,7 @@ setname='iotltsc226211'
 diskfilename='W11IoTLTSC226211.qcow2'
 setfileurl='https://pixeldrain.com/api/file/LJmGhucb?download'
 setfileurl2='https://archive.org/download/windows-11-io-t-ltsc-22621.1.7znbab/Windows%2011%20IoT%20LTSC%2022621.1.7z'
-setfileurl3='http://go.anbui.ovh:8000/Termux/11/Windows%2011%20IoT%20LTSC%2022621.1.7z'
+setfileurl3='https://go.anbui.ovh/termux/qemu/windows/nt10/Windows%2011%20IoT%20LTSC%2022621.1.7z'
 setqemucommand='qemu-system-x86_64 -M q35 -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M,slots=4,maxmem=4097M -object memory-backend-ram,size=1024M,id=m0 -numa node,nodeid=0,memdev=m0,cpus=0 -object memory-backend-ram,size=1024M,id=m1 -numa node,nodeid=1,memdev=m1,cpus=1 -object memory-backend-ram,size=1024M,id=m2 -numa node,nodeid=2,memdev=m2,cpus=2 -object memory-backend-ram,size=1024M,id=m3 -numa node,nodeid=3,memdev=m3,cpus=3 -drive file=/storage/emulated/0/VM/iotltsc226211/W11IoTLTSC226211.qcow2,aio=threads,cache=unsafe,if=none,id=hda -device virtio-blk-pci,drive=hda -device qxl-vga,vgamem_mb=128 -device ich9-intel-hda -device hda-duplex -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -drive file=/storage/emulated/0/VM/iotltsc226211/OVMF.fd,format=raw,readonly=on,if=pflash -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -device intel-iommu -monitor stdio -vnc :2'
 osname="Windows_11_IoT_LTSC_build_22621.1"
 export setname
