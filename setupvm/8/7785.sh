f842bc05ed5171d3d35c1066a4e27ae633080bb7
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-07-12 20:17:58 +0700

    Update 7785.sh

--- setupvm/8/7785.sh
+++ setupvm/8/7785.sh
@@ -1,9 +1,9 @@
 cd
 setname='87785'
 diskfilename='W87785.qcow2'
-setfileurl='https://go.anbui.ovh/termux/qemu/windows/nt6/Windows%208%20build%207785.7z'
-setfileurl2=''
-setfileurl3=''
+setfileurl='https://pixeldrain.com/api/file/aG28unRW?download'
+setfileurl2='https://archive.org/download/windows-8-build-7785.7znbsb/Windows%208%20build%207785.7z'
+setfileurl3='https://go.anbui.ovh/termux/qemu/windows/nt6/Windows%208%20build%207785.7z'
 setqemucommand='qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu qemu64 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M -drive file=/storage/emulated/0/VM/87785/W87785.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device ich9-intel-hda -device hda-duplex -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -rtc base=2010-07-21t00:00:00 -drive file=/storage/emulated/0/VM/87785/OVMF.fd,format=raw,readonly=on,if=pflash -drive file=/storage/emulated/0/VM/87785/RELEASEX64_OVMF_VARS.fd,format=raw,if=pflash -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -monitor stdio -vnc :2'
 osname="Windows_8_build_7785"
 export setname
