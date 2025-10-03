3071276c6dbf1b830b103d8f3667a98b096ff296
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-10-03 17:01:37 +0700

    Update startsetup6.sh

--- setupvm/ce/startsetup6.sh
+++ setupvm/ce/startsetup6.sh
@@ -3,7 +3,7 @@ setname='wce6'
 diskfilename='WCE60.vhd'
 #setfileurl='https://pixeldrain.com/api/file/pk1WVUZi?download'
 setfileurl='https://archive.org/download/vectras-vm-free-open-source-roms-nbab-end-of-april-2025/Windows%20CE%206.0.7z'
-setfileurl1='https://go.anbui.ovh/termux/qemu/windows/ce/Windows%20CE%206.0.7z'
+setfileurl2='https://go.anbui.ovh/termux/qemu/windows/ce/Windows%20CE%206.0.7z'
 setqemucommand='qemu-system-x86_64 -usb -cpu pentium3 -smp sockets=1,cores=1,threads=1 -overcommit mem-lock=off -m 128M -hda /storage/emulated/0/VM/wce6/WCE60.vhd -vga cirrus -device sb16 -device e1000-82544gc,netdev=n0 -netdev user,id=n0 -accel tcg,thread=single,tb-size=1024 -monitor stdio -vnc :2'
 osname="Windows_CE_6.0"
 distro="debian-bookworm"
