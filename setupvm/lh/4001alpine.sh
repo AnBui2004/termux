603dc98c6a58a3e082e58287482918535c062a3e
An Bui <91354810+AnBui2004@users.noreply.github.com>
2025-10-17 00:57:45 +0700

    Update 4001alpine.sh

--- setupvm/lh/4001alpine.sh
+++ setupvm/lh/4001alpine.sh
@@ -6,7 +6,7 @@ setfileurl2='https://archive.org/download/windows-longhorn-4001.7znbab/Windows%2
 setfileurl3='https://go.anbui.ovh/termux/qemu/windows/nt6/Windows%20Longhorn%204001.7z'
 setqemucommand='qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu pentium3 -smp sockets=1,cores=1,threads=1 -overcommit mem-lock=off -m 1024M -drive file=/storage/emulated/0/VM/wl4001/WL4001.qcow2,aio=threads,cache=unsafe -vga cirrus -device ac97 -device rtl8139,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -monitor stdio -vnc :2 -rtc base=2002-12-04t00:00:00'
 osname="Windows_Longhorn_4001"
-distro="debian-bookworm"
+distro="alpine-3.22.2"
 export distro
 export setname
 export diskfilename
