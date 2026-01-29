acc3e2a92e7456c92fb4df1a17bb6e5036ddfebc
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-01-29 18:02:28 +0700

    Update

--- setupvm/8/8400.sh
+++ setupvm/8/8400.sh
@@ -1,9 +1,9 @@
 cd
 setname='88400'
 diskfilename='W88400.qcow2'
-setfileurl='https://pixeldrain.com/api/file/vgmw46W2?download'
-setfileurl2='https://archive.org/download/windows-8-build-8400.7znbab/Windows%208%20build%208400.7z'
-setfileurl3='https://go.anbui.ovh/termux/qemu/windows/nt6/Windows%208%20build%208400.7z'
+#setfileurl='https://pixeldrain.com/api/file/vgmw46W2?download'
+setfileurl='https://archive.org/download/windows-8-build-8400.7znbab/Windows%208%20build%208400.7z'
+setfileurl2='https://go.anbui.ovh/termux/qemu/windows/nt6/Windows%208%20build%208400.7z'
 setqemucommand='qemu-system-x86_64 -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M -drive file=/storage/emulated/0/VM/88400/W88400.qcow2,aio=threads,cache=unsafe -device qxl-vga,vgamem_mb=128 -device ich9-intel-hda -device hda-duplex -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -rtc base=2012-05-18t00:00:00 -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -monitor stdio -vnc :2'
 osname="Windows_8_build_8400"
 distro="debian-bookworm"
@@ -12,6 +12,6 @@ export setname
 export diskfilename
 export setfileurl
 export setfileurl2
-export setfileurl3
+#export setfileurl3
 export setqemucommand
 export osname
