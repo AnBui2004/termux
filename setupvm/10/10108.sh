21f5dfbd82bdbd01600d7419fdac3e0224b5d84a
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-02-10 21:34:26 +0700

    Update 10108.sh

--- setupvm/10/10108.sh
+++ setupvm/10/10108.sh
@@ -1,9 +1,9 @@
 cd
 setname='1010108'
 diskfilename='W1010108.qcow2'
-setfileurl='https://pixeldrain.com/api/file/LNe6VNgg?download'
-setfileurl2='https://archive.org/download/windows-10-build-10108.7znbab/Windows%2010%20build%2010108.7z'
-setfileurl3='https://go.anbui.ovh/termux/qemu/windows/nt10/Windows%2010%20build%2010108.7z'
+#setfileurl='https://pixeldrain.com/api/file/LNe6VNgg?download'
+setfileurl='https://archive.org/download/windows-10-build-10108.7znbab/Windows%2010%20build%2010108.7z'
+setfileurl2='https://go.anbui.ovh/termux/qemu/windows/nt10/Windows%2010%20build%2010108.7z'
 setqemucommand='qemu-system-x86_64 -M q35 -device qemu-xhci -device usb-tablet -device usb-kbd -cpu qemu64 -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M -drive file=/storage/emulated/0/VM/1010108/W1010108.qcow2,aio=threads,cache=unsafe -device qxl-vga,ram_size_mb=16,vgamem_mb=16,vram64_size_mb=16 -device ich9-intel-hda -device hda-duplex -device e1000e,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -rtc base=2015-04-24t00:00:00 -drive file=/storage/emulated/0/VM/1010108/OVMF.fd,format=raw,readonly=on,if=pflash -drive file=/storage/emulated/0/VM/1010108/RELEASEX64_OVMF_VARS.fd,format=raw,if=pflash -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -device intel-iommu -monitor stdio -vnc :2'
 osname="Windows_10_build_10108"
 distro="debian-bookworm"
@@ -12,7 +12,7 @@ export setname
 export diskfilename
 export setfileurl
 export setfileurl2
-export setfileurl3
+#export setfileurl3
 export setqemucommand
 export osname
 export forceqemu7
