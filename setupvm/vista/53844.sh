9a43108ced89cf524ca501c6a1b1ba2f08e16d79
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-03-08 13:33:14 +0700

    Update 53844.sh

--- setupvm/vista/53844.sh
+++ setupvm/vista/53844.sh
@@ -6,7 +6,7 @@ setfileurl2=''
 setfileurl3=''
 setqemucommand='qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu core2duo -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M -drive file=/storage/emulated/0/VM/WV53844/WV5384.4.qcow2,aio=threads,cache=unsafe -vga std -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -monitor stdio -vnc :2 -rtc base=2006-05-18t00:00:00'
 osname="Windows_Vista_build_5384.4"
-distro="debian-bookworm"
+distro="debian"
 export distro
 export setname
 export diskfilename
