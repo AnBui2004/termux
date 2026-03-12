fec3990ea012c37a8e32684ffa6909204e458c31
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-03-12 10:10:07 +0700

    Update 53844.sh

--- setupvm/vista/53844.sh
+++ setupvm/vista/53844.sh
@@ -1,9 +1,9 @@
 cd
 setname='wv53844'
 diskfilename='WV5384.4.qcow2'
-setfileurl='http://192.168.1.110/termux/qemu/windows/nt6/Windows%20Vista%205384.4.7z'
-setfileurl2=''
-setfileurl3=''
+setfileurl='https://pixeldrain.com/api/file/sTpbg2Dq?download'
+setfileurl2='https://archive.org/download/windows-vista-5384.4.7znbab/Windows%20Vista%205384.4.7z'
+setfileurl3='https://go.anbui.ovh/termux/qemu/windows/nt6/Windows%20Vista%205384.4.7z'
 setqemucommand='qemu-system-x86_64 -usb -device usb-tablet -device usb-kbd -cpu core2duo -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M -drive file=/storage/emulated/0/VM/WV53844/WV5384.4.qcow2,aio=threads,cache=unsafe -vga std -device ac97 -device e1000,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -monitor stdio -vnc :2 -rtc base=2006-05-18t00:00:00'
 osname="Windows_Vista_build_5384.4"
 distro="debian"
