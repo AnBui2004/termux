ed591ade2b98360c70dc7f99435b58e3192f5e8b
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-06-12 21:09:54 +0700

    Test

--- proot-distro-test2/kali-x11/install1.sh
+++ proot-distro-test2/kali-x11/install1.sh
@@ -23,17 +23,17 @@ echo -e '\e[1;37m[i] Installing packages...\e[0m'
 apt update
 yes y | apt upgrade -y
 apt install x11-repo -y
-apt install proot-distro termux-x11 -y
+apt install proot-distro termux-x11 aria2 -y
 
 echo -e '\e[1;37m[i] Installing Linux...\e[0m'
 proot-distro install kalilinux/kali-rolling
 
 echo -e '\e[1;37m[i] Just a sec...\e[0m'
-mkdir -p $PREFIX/var/lib/proot-distro/installed-rootfs/kali-rolling/home/kaliroom
-cd $PREFIX/var/lib/proot-distro/installed-rootfs/kali-rolling/etc/profile.d
-aria2c -o installkali.sh https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/proot-distro-test1/kali-x11/install2.sh
+mkdir -p $PREFIX/var/lib/proot-distro/containers/kali-rolling/home/kaliroom
+cd $PREFIX/var/lib/proot-distro/containers/kali-rolling/etc/profile.d
+aria2c -o installkali.sh https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/proot-distro-test2/kali-x11/install2.sh
 chmod +x installkali.sh
-cd $PREFIX/var/lib/proot-distro/installed-rootfs/kali-rolling/root
+cd $PREFIX/var/lib/proot-distro/containers/kali-rolling/root
 echo "sed -i \"/com.termux.x11/d\" /home/kaliroom/.profile" > "startkali.sh"
 echo "sed -i \"/termux-x11/d\" /home/kaliroom/.profile" >> "startkali.sh"
 echo 'echo "am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity" >> /home/kaliroom/.profile' >> startkali.sh
@@ -43,10 +43,10 @@ echo "su - kaliroom" >> startkali.sh
 echo "" >> startkali.sh
 chmod +x startkali.sh
 cd
-echo "sed -i \"/com.termux.x11/d\" $PREFIX/var/lib/proot-distro/installed-rootfs/kali-rolling/home/kaliroom/.profile" > "startkali.sh"
-echo "sed -i \"/termux-x11/d\" $PREFIX/var/lib/proot-distro/installed-rootfs/kali-rolling/home/kaliroom/.profile" >> "startkali.sh"
-echo "echo 'am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity' >> $PREFIX/var/lib/proot-distro/installed-rootfs/kali-rolling/home/kaliroom/.profile" >> startkali.sh
-echo "echo 'termux-x11 -xstartup \"fluxbox\" &' >> $PREFIX/var/lib/proot-distro/installed-rootfs/kali-rolling/home/kaliroom/.profile" >> startkali.sh
+echo "sed -i \"/com.termux.x11/d\" $PREFIX/var/lib/proot-distro/containers/kali-rolling/home/kaliroom/.profile" > "startkali.sh"
+echo "sed -i \"/termux-x11/d\" $PREFIX/var/lib/proot-distro/containers/kali-rolling/home/kaliroom/.profile" >> "startkali.sh"
+echo "echo 'am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity' >> $PREFIX/var/lib/proot-distro/containers/kali-rolling/home/kaliroom/.profile" >> startkali.sh
+echo "echo 'termux-x11 -xstartup \"fluxbox\" &' >> $PREFIX/var/lib/proot-distro/containers/kali-rolling/home/kaliroom/.profile" >> startkali.sh
 echo "export PULSE_SERVER=127.0.0.1" >> startkali.sh
 echo "pulseaudio --start --load="module-native-protocol-tcp auth-ip-acl=127.0.0.1 auth-anonymous=1" --exit-idle-time=-1" >> startkali.sh
 echo "pactl load-module module-aaudio-sink" >> startkali.sh
