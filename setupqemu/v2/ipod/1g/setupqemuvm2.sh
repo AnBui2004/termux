a98fb43e40fe8f32c04de0b1a5dd224268ef30c6
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-06-17 23:37:24 +0700

    Update setupqemuvm2.sh

--- setupqemu/v2/ipod/1g/setupqemuvm2.sh
+++ setupqemu/v2/ipod/1g/setupqemuvm2.sh
@@ -8,7 +8,7 @@ if [ -e "/usr/local/bin/qemu-system-arm-aipt1g" ]; then
 else
     apt update
     apt upgrade -y
-    apt install libx11-6 libxxf86vm1 libgl1 libpixman-1-0 libepoxy0 libsnappy1v5 liblzo2-2 libpng16-16 libjpeg62 libgnutls30 libsdl2-2.0-0 libvdeplug2 libvte-2.91-0 libsdl2-image-2.0-0 libjack0 libsndio7.0 libcurl4 libssh-4 libspice-server1 libslirp0 libvirglrenderer1 libusbredirparser1 pulseaudio libcapstone4 libbpf0 libusb-1.0-0 liburing1 libfuse3-3 libaio1 libnfs13 -y
+    apt install aria2 libx11-6 libxxf86vm1 libgl1 libpixman-1-0 libepoxy0 libsnappy1v5 liblzo2-2 libpng16-16 libjpeg62 libgnutls30 libsdl2-2.0-0 libvdeplug2 libvte-2.91-0 libsdl2-image-2.0-0 libjack0 libsndio7.0 libcurl4 libssh-4 libspice-server1 libslirp0 libvirglrenderer1 libusbredirparser1 pulseaudio libcapstone4 libbpf0 libusb-1.0-0 liburing1 libfuse3-3 libaio1 libnfs13 -y
     tar -xzvf qemu-system.tar.gz -C /
     tar -xzvf qemu-tools.tar.gz -C /
     rm qemu-system.tar.gz
@@ -17,7 +17,7 @@ fi
 clear
 echo -e '\e[1;37m[i] Just a sec...\e[0m'
 cd
-curl -o "start"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setupqemu//v2/ipod/1g/startqemuvm.sh
+aria2c -o "start"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setupqemu//v2/ipod/1g/startqemuvm.sh
 chmod +rwx "start"$setname".sh"
 clear
 echo -e '\e[1;37m[i] Done!\e[0m'
