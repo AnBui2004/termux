808864cb5c9892ec52c9fa99f56144be564ac252
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-07-27 22:31:36 +0700

    Update setupqemuvm2.sh

--- setupqemuv2/setupqemuvm2.sh
+++ setupqemuv2/setupqemuvm2.sh
@@ -44,6 +44,8 @@ if [ -f "/storage/emulated/0/VM/"$setname"/"$diskfilename"" ]; then
     sed -i -e "1isetfileurl2='$setfileurl2'" "start"$setname".sh"
     sed -i -e "1isetfileurl3='$setfileurl3'" "start"$setname".sh"
 
+    sed -i '/^osname=/ s/_/ /g' "start"$setname".sh"
+
     clear
     echo -e '\e[1;37m[i] Done!\e[0m'
     echo -e '\e[1;37m-\e[0m'
