6803e85835776e67d09d303223411104ad83200b
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-07-27 22:20:00 +0700

    Update

---
+++ setupqemuv2/setupqemuvm2.sh
@@ -0,0 +1,80 @@
+clear
+echo -e '\e[1;37m[i] Installing packages...\e[0m'
+sed -i "/setup"$setname"/d" /etc/profile
+if [ -e "/usr/local/bin/qemu-system-x86_64" ]; then
+    echo "qemu-system-x86_64 is already installed."
+else
+    apk update
+    apk add aria2 nano libslirp curl libnfs libssh vde2-libs pixman sdl2 sdl2_image libepoxy virglrenderer gtk+3.0 libusb usbredir pulseaudio jack libaio liburing fuse3-libs snappy lzo zlib capstone libcbor keyutils-libs ndctl rdma-core linux-pam
+    architecture=$(uname -m)
+    if [[ "$architecture" =~ "aarch64" ]]; then
+        qemuurl="https://github.com/AnBui2004/Vectras-VM-Emu-Android/releases/download/4.4.7/base-qemu-7.2.22-3dfx-july-2026-vectras-vm-arm64-v8a.tar.gz"
+    elif [[ "$architecture" =~ "x86_64" ]]; then
+        qemuurl="https://github.com/AnBui2004/Vectras-VM-Emu-Android/releases/download/4.4.7/base-qemu-7.2.22-3dfx-july-2026-vectras-vm-x86_64.tar.gz"
+    elif [[ "$architecture" =~ "arm" ]]; then
+        qemuurl="https://github.com/AnBui2004/Vectras-VM-Emu-Android/releases/download/4.4.7/base-qemu-7.2.22-july-2026-vectras-vm-armeabi-v7a.tar.gz"
+    elif [[ "$architecture" =~ "x86" ]]; then
+        qemuurl="https://github.com/AnBui2004/Vectras-VM-Emu-Android/releases/download/4.4.7/base-qemu-7.2.22-july-2026-vectras-vm-x86.tar.gz"
+    elif [[ "$architecture" =~ "i386" || "$architecture" =~ "i686" ]]; then
+        qemuurl="https://github.com/AnBui2004/Vectras-VM-Emu-Android/releases/download/4.4.7/base-qemu-7.2.22-july-2026-vectras-vm-x86.tar.gz"
+    fi
+    clear
+    echo -e '\e[1;37m[i] Downloading QEMU...\e[0m'
+    aria2c -x 4 --async-dns=false --disable-ipv6 --check-certificate=false -o setup.tar.gz "$qemuurl"
+    clear
+    echo -e '\e[1;37m[i] Installing QEMU...\e[0m'
+    tar -xzvf setup.tar.gz -C /
+    rm setup.tar.gz
+    chmod 755 /usr/local/bin/qemu*
+fi
+export PULSE_SERVER=127.0.0.1
+clear
+echo -e '\e[1;37m[i] Just a sec...\e[0m'
+if [ -f "/storage/emulated/0/VM/"$setname"/"$diskfilename"" ]; then
+    cd
+    curl -o "start"$setname".sh" https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setupqemu/v2/startqemuvm.sh
+    chmod +rwx "start"$setname".sh"
+
+    # Use a loop to insert variables into the script
+    declare -A variables=(
+        [setfileurl3]="$setfileurl3"
+        [setfileurl2]="$setfileurl2"
+        [setfileurl]="$setfileurl"
+        [setname]="$setname"
+        [osname]="$osname"
+    )
+
+    for key in "${!variables[@]}"; do
+        sed -i -e "1i$key='${variables[$key]}'" "start"$setname".sh"
+    done
+
+    sed -i '/^osname=/ s/_/ /g' "start"$setname".sh"
+
+    clear
+    echo -e '\e[1;37m[i] Done!\e[0m'
+    echo -e '\e[1;37m-\e[0m'
+    echo -e "\e[1;37mUse this command to run: \"./start"$setname".sh\"\e[0m"
+    echo -e '\e[1;37m-\e[0m'
+    echo -e '\e[1;37mThe necessary files are in the VM folder on your phone. Please do not delete the files there if you still use them.\e[0m'
+    echo -e '\e[1;37m-\e[0m'
+    if [ -n "$notes" ]; then
+        printf "\n"
+        curl $notes
+        printf "\n"
+    fi
+else
+    sed -i "/start"$setname"/d" /etc/profile
+    cd
+    rm start"$setname"vm.sh
+    rm -r /storage/emulated/0/VM/"$setname"
+    rm start"$setname".sh
+    rm /data/data/com.termux/files/home/start"$setname".sh
+    clear
+    echo -e "\e[0;33m[!] Unsuccessful!"
+    echo -e "\e[1;37m-\e[0m"
+    echo -e "\e[91mError! An error occurred. Please try again later."
+fi
+rm "setup"$setname".sh" >/dev/null 2>&1
+rm "/data/data/com.termux/files/home/setup1.sh" >/dev/null 2>&1
+rm "/data/data/com.termux/files/home/setup2.sh" >/dev/null 2>&1
+exit
---
+++ setupqemuv2/test.sh
@@ -0,0 +1,16 @@
+cd
+setname='test'
+diskfilename='disk.qcow2'
+setfileurl='https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setupqemuv2/disk.7z'
+setfileurl='https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setupqemuv2/disk.7z'
+setfileurl2='https://raw.githubusercontent.com/AnBui2004/termux/refs/heads/main/setupqemuv2/disk.7z'
+setqemucommand='qemu-system-x86_64 -m 128M -hda /sdcard/VM/test/disk.qcow2 -monitor stdio -vnc :2'
+osname="Test_test"
+export distro
+export setname
+export diskfilename
+export setfileurl
+export setfileurl2
+export setfileurl3
+export setqemucommand
+export osname
