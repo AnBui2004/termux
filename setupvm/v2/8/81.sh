4b32596cfac6d22fcdc8b589e78c86ffb3f356fd
An Bui <91354810+AnBui2004@users.noreply.github.com>
2026-07-28 22:34:26 +0700

    Update 81.sh

--- setupvm/v2/8/81.sh
+++ setupvm/v2/8/81.sh
@@ -1,9 +1,9 @@
 cd
 setname='81'
 diskfilename='W81.qcow2'
-setfileurl='http://192.168.1.110/Windows%208.1.7z'
-setfileurl2=''
-setfileurl3=''
+setfileurl='https://pixeldrain.com/api/file/iPjZcduX?download'
+setfileurl2='https://archive.org/download/termux-images-july-2026-nbab/Windows%208.1.7z'
+setfileurl3='https://go.anbui.ovh/termux/qemu/windows/nt6/Windows%208.1.7z'
 setqemucommand='qemu-system-x86_64 -M q35,hpet=off -device qemu-xhci -device usb-tablet -device usb-kbd -cpu core2duo -smp sockets=1,cores=4,threads=1 -overcommit mem-lock=off -m 4096M -drive file=/storage/emulated/0/VM/81/W81.qcow2,aio=threads,cache=writeback,l2-cache-size=2M -device qxl-vga,vgamem_mb=128 -device ich9-intel-hda -device hda-duplex -device virtio-net-pci,netdev=n0 -netdev user,id=n0 -accel tcg,thread=multi,tb-size=2048 -drive file=/storage/emulated/0/VM/81/edk2-x86_64-code.fd,format=raw,readonly=on,if=pflash -drive file=/storage/emulated/0/VM/81/edk2-i386-vars.fd,format=raw,if=pflash -device virtio-balloon-pci -device virtio-serial-pci -device virtio-rng-pci -device intel-iommu -monitor stdio -vnc :2'
 osname="Windows_8.1"
 export distro
